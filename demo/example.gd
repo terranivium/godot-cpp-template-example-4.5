614158808eca7b5e1781082593f532a6237488d1
terran <wesleyscottuk@gmail.com>
2025-10-18 23:45:47 +0100

    render blue cube

--- demo/example.gd
+++ demo/example.gd
@@ -1,6 +1,13 @@
-extends Node
+extends Node3D
 
 
 func _ready() -> void:
-	var example := ExampleClass.new()
-	example.print_type(example)
+	# Create a CubeRenderer (your C++ class)
+	var cube := CubeRenderer.new()
+	add_child(cube)
+	
+	# Set some properties
+	cube.rotation_speed = 2.0
+	cube.cube_color = Color.BLUE
+	
+	print("CubeRenderer created and added to scene!")
